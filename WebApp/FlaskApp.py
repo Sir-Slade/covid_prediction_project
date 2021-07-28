4069686e82e5957517a640611b44e390ae85311d
Jeshua <shark12_venom@hotmail.com>
2021-07-28 23:45:53 +0100

    Data wrangling has been done to the input data from the form. However still need to find a better way to convert from string to boolean

--- WebApp/FlaskApp.py
+++ WebApp/FlaskApp.py
@@ -17,16 +17,32 @@ def home():
 #Note: The value of route and the name of the function must be the same
 @app.route('/predictHTML',methods=['POST'])
 def predictHTML():
+    ### Test input (which results in positiv if we do not use the imputer and negative if we do)
+    ### {'wheezes': 'True', 'ctab': 'False', 'high_risk_exposure_occupation': 'True', 'cough': 'False', 'loss_of_smell': 'True', 'loss_of_taste': 'False', 'muscle_sore': 'True', 'headache': 'False', 'fatigue': 'True', 'diabetes': 'False', 'pam': '1', 'rr': '2', ###'pulse': '3', 'temperature': '4', 'days_since_symptom_onset': '5'}
     
-    for i in request.form.values():
-        print(i)
-    int_features = [x for x in request.form.values()]
-    ##final_features = [np.array(int_features)]
-    ##prediction = model.predict(final_features)
-    return render_template('index.html', prediction_text=str(int_features))
+    categorical_cols = ['wheezes', 'ctab', 'high_risk_exposure_occupation', 'cough',
+       'loss_of_smell', 'loss_of_taste', 'muscle_sore', 'headache', 'fatigue',
+       'diabetes']
+    numerical_cols = ['pam', 'rr', 'pulse', 'temperature',
+       'days_since_symptom_onset']
+    ordered_features = ["wheezes", "ctab", "pam", "rr", "pulse", "temperature", "high_risk_exposure_occupation", "cough", "loss_of_smell", "muscle_sore", "loss_of_taste", "headache", "days_since_symptom_onset", "fatigue", "fever", "diabetes"]
+    data = pd.DataFrame(dict(request.form), index=[0]) #Because we are just prediction one result, the index is 0
+    data[categorical_cols] = data[categorical_cols].astype("bool")
+    data[numerical_cols] = data[numerical_cols].astype("float")    
+    data["fever"] = data["temperature"] >= 38
+    
+    print(data.iloc[0])
+    imputer.transform(data) #Note: If I use the imputer, the result becomes negative. However if I dont, the result is positive with the same values???
+    
+    print(data.iloc[0])
+    data = data[ordered_features] #Because the order of the features matters in sklearn (does not remeber the names of the columns) and this statement will get the dataframe in the same order as 'ordered_features'
+    
+    
+    prediction = model.predict(data)
+    #return render_template('index.html', prediction_text=str(int_features))
     ##output = round(prediction[0], 2)
 
-    #return render_template('index.html', prediction_text='Sales should be $ {}'.format(output))
+    return render_template('index.html', prediction_text='Your result is likely {}'.format(prediction[0]))
 
 @app.route('/predict', methods=['POST'])
 def predict():
